f18e4fb6f3e8290c7944deda7cbf97f02290b71e
maksymilianwojewoda <maksymilianwojewoda@gmail.com>
2020-05-19 13:28:04 +0200

    returning the welcome screen

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const fs = require('fs');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('hello world');
+  // TODO: tutaj chcemy zwrócić użytkownikowi stronę startową (ekran logowania)
+  fs.readFile(`${__dirname}/public/index.html`, (err, data ) => {
+    res.send(data);
+  })
 });
+
 
 server.listen(3000);
