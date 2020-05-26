04f6bb642a6e8c546b1fc5ad654b8cf034876ac0
maksymilianwojewoda <maksymilianwojewoda@gmail.com>
2020-05-26 14:46:54 +0200

    moving player

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const fs = require('fs');
 const server = express();
 
 server.get('/', (req, res) => {
   // TODO: tutaj chcemy zwrócić użytkownikowi stronę startową (ekran logowania)
-  fs.readFile(`${__dirname}/public/index.html`, (err, data ) => {
-    res.send(data);
-  })
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
 
